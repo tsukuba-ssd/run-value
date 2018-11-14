fb17fa0d6fb0495fb397da8b45876b5ebc013620
hottayuusuke <38587433+hottayuusuke@users.noreply.github.com>
2018-11-14 13:10:42 +0900

    Add files via upload

--- run_v@lue.rb
+++ run_v@lue.rb
@@ -1,7 +1,7 @@
 require 'csv'
 
 #csvのデータを読み込む
-csv_data = CSV.read('gamedate_test1.csv', headers: true)
+csv_data = CSV.read('gamedata.csv', headers: true)
 
 #score,occurはhash(keyを24パターン　valueを0とする)をpatternは配列を作る
 score = {"0000" => 0, "1000" => 0, "2000" => 0, "0001" => 0, "1001" => 0, "2001" => 0, "0010" => 0, "1010" => 0, "2010" => 0, "0100" => 0, "1100" => 0, "2100" => 0, "0011" => 0, "1011" => 0, "2011" => 0, "0110" => 0, "1110" => 0, "2110" => 0, "0101" => 0, "1101" => 0, "2101" => 0, "0111" => 0, "1111" => 0, "2111" => 0 }
@@ -13,34 +13,34 @@ pattern = []
 pattern.push("0000")
 
 #状況が変化したかどうかの判断材料にするためnow,prevとし定義する
-now = '0000'
-prev = '0000'
+now = "0000"
+prev = "0000"
 
 #csvのファイルを読み込みデータを回す
 csv_data.each do |data|
 
 #それぞれのデータと照合して数値を代入する
-	  if data["アウト"] == "0"
+	  if data['out'] == '0'
 			out = 0
-		elsif data["アウト"] == "1"
+		elsif data['out'] == '1'
 			out = 1
-		elsif data["アウト"] == "2"
+		elsif data['out'] == '2'
 			out = 2
 		end
 
-		if data["一走打順"] == "0"
+		if data['一走状況'] == '0'
 			runners1 = 0
 		else
 			runners1 = 1
 		end
 
-		if data["二走打順"] == "0"
+		if data['二走状況'] == '0'
 			runners2 = 0
 		else
 			runners2 = 1
 		end
 
-		if data["三走打順"] == "0"
+		if data['三走状況'] == '0'
 			runners3 = 0
 		else
 			runners3 = 1
@@ -84,7 +84,7 @@ csv_data.each do |data|
 			#patternをリセットする
 			pattern = []
 #回が継続していた場合 #nowが0000か比較する
-		elsif now == 00000
+elsif now == "00000"
 			#occure_hashのkey0000のvalueに1足す
 			occur.store("0000", occur["0000"] + 1)
 			#pattern配列に0000を追加する
@@ -104,3 +104,5 @@ csv_data.each do |data|
 
 	end
 end
+
+p score
